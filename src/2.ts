1e40b0a27f538dd944628e56919a77f5c3a5a037
v.karakai <v.karakai@pin-up.tech>
2025-04-05 13:36:46 +0300

    HW-7 task 2

--- src/2.ts
+++ src/2.ts
@@ -3,7 +3,7 @@
 
   Клас Employee повинен включати:
 
-  властивість name, яка буде доступна всім.
+  Властивість name, яка буде доступна всім.
   властивість department, яка буде доступна лише всередині класу Employee.
   salary, яке буде доступне лише всередині класу Employee та його підкласів.
 
@@ -15,10 +15,9 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -32,8 +31,9 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
