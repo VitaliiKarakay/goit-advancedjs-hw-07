7b9fdf4602faf8caddddc95b6c77829cd7ccad54
v.karakai <v.karakai@pin-up.tech>
2025-04-05 13:42:50 +0300

    HW-7 task 4

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,62 @@
+class Key {
+    private readonly signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private readonly key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`${person.getKey().getSignature()} entered the house.`);
+        } else {
+            console.log('The door is closed.');
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('The door is now open.');
+        } else {
+            console.log('The key does not match.');
+        }
+    }
+}
+
 const key = new Key();
+const key2 = new Key();
+const notMyHouse = new MyHouse(key2);
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -7,5 +65,7 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+notMyHouse.openDoor(key);
+notMyHouse.comeIn(person);
 
 export {};
